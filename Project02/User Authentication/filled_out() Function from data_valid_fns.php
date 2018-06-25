590ceb8d7ca24bd3f8777fcb0104464e0d71f684
robby06 <40376728+robby06@users.noreply.github.com>
2018-06-24 21:04:10 -0700

    Work in progress

---
+++ Project02/User Authentication/filled_out() Function from data_valid_fns.php
@@ -0,0 +1,11 @@
+<?php
+function filled_out($form_vars) {
+  // test that each variable has a value
+  foreach ($form_vars as $key => $value) {
+     if ((!isset($key)) || ($value == '')) {
+        return false;
+     }
+  }
+  return true;
+}
+?>
